d68e155a4e2f3b038329c5dc3b13b73c0e59dca9
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-04-08 10:04:06 -0300

    Finalizado o desafio01

---
+++ src/main/java/desafio01/Tipos.java
@@ -0,0 +1,13 @@
+package desafio01;
+
+public class Tipos {
+
+	public byte aByte;
+	public short aShort;
+	public int aInt;
+	public long aLong;
+	public float aFloat;
+	public double aDouble;
+	public char aChar;
+	
+}
